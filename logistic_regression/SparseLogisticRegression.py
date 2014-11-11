3b424211429aacecc19ef7b6cbf765a89e0d5ae5
Lam Tran <lam@tradesy.com>
2014-11-10 18:38:58 -0800

    mini batching added with shuffle after each epoch

--- logistic_regression/SparseLogisticRegression.py
+++ logistic_regression/SparseLogisticRegression.py
@@ -22,7 +22,6 @@ https://www.kaggle.com/users/185835/tinrtgu
 My contributions are as follows: de-couping the code from the Critero Competition,
 making it look like sklearn, adding l1/l2 regularization and minibatching
 
-
 Dependencies: 
 
 Numpy
@@ -36,7 +35,7 @@ class SparseLogisticRegression(object):
     
     #Verbose mode determines the logloss at every step and occasionally prints it out. 
     #Setting verbose = False will give performance gains
-    def __init__(self, dimension_size, learning_rate=.1, max_epochs=1, l1_ratio=0., l2_ratio=.0, verbose=True):
+    def __init__(self, dimension_size, learning_rate=.1, batch_size=10, max_epochs=1, l1_ratio=0., l2_ratio=.0, verbose=True):
         self.D = dimension_size
         self.learning_rate = learning_rate
         self.max_epochs = max_epochs
@@ -44,22 +43,36 @@ class SparseLogisticRegression(object):
         self.l2_ratio = l2_ratio
         self.verbose = verbose
         
+        self.batch_size = batch_size
+        
         self.w = [0.] * self.D  # weights
         self.n = [0.] * self.D  # number of times we've encountered a feature
+        self.n_sparse = set()
+        self.printHyperParams()
+ 	
+    def printHyperParams(self):
+        print "batch_size:\t%s\nlearning_rate:\t%s\nmax_epochs:\t%s" % (self.batch_size,self.learning_rate, self.max_epochs)
         
-        return 
-    
     def logloss(self,p, y):
         p = max(min(p, 1. - 10e-12), 10e-12)
         return -log(p) if y == 1. else -log(1. - p)
     
-    def update_w(self,x, p, y):
+    def calc_gradient(self, x, p, y):
+	dw = (p - y) * self.learning_rate #/ (sqrt(self.n[i]) + 1.)
         for i in x:
             # alpha / (sqrt(n) + 1) is the adaptive learning rate heuristic
             # (p - y) * x[i] is the current gradient
             # note that in our case, if i in x then x[i] = 1
-            self.w[i] -= (p - y) * self.learning_rate / (sqrt(self.n[i]) + 1.)
             self.n[i] += 1.
+            self.n_sparse.add(i)
+            
+        return dw
+    
+    def update_w(self,dw):
+	self.n_sparse = set(self.n_sparse)
+        for i in self.n_sparse:
+            self.w[i] -= dw / sqrt(self.n[i]/self.batch_size)
+	self.n_sparse = set()
     
     def get_p(self,x):
         wTx = 0.
@@ -68,24 +81,40 @@ class SparseLogisticRegression(object):
         return 1. / (1. + exp(-max(min(wTx, 20.), -20.)))  # bounded sigmoid
     
     def fit(self,X, Y):
+	Y_len = len(Y)
+	if Y != numpy.ndarray:
+	    Y = numpy.array(Y)
+	if Y.shape != (Y_len,1):
+	    Y = Y.reshape(Y_len,1)	
+
         if X != numpy.ndarray:
             X = numpy.array(X)
-        rows = X.shape[0]
-        verbose_freq = ceil(rows/10)
+	data = numpy.hstack((Y,X))
+        rows = data.shape[0]
+        verbose_freq = ceil(rows/5)
+        
+        num_batches = int(ceil(rows/self.batch_size))
         for current_epoch in range(self.max_epochs):
             loss = .0
             if self.verbose == True:
                 print('%s\tepoch: %s' % (datetime.now(), current_epoch))
-            for index in range(rows):
-                p = self.get_p(X[index])
-                self.update_w(X[index, :], p, Y[index])
                 
-                if self.verbose == True:
-                    loss += self.logloss(p,Y[index])
-                    if index % verbose_freq == 0 and index > 0:
-                        print('%s\tencountered: %d\tcurrent logloss: %f' % (datetime.now(), index, loss/index))
+            for batch_id in range(num_batches):
+                total_dw = 0.
+                for batch_index in range(self.batch_size):
+                    index = (batch_id * self.batch_size) + batch_index
+                    p = self.get_p(data[index,1:])
+                    total_dw += self.calc_gradient(data[index,1:], p, data[index,0])
+                    
+                    if self.verbose == True:
+                        loss += self.logloss(p,data[index,0])
+                        if index % verbose_freq == 0 and index > 0:
+                            print('%s\tencountered: %d\tcurrent logloss: %f' % (datetime.now(), index, loss/index))
+                
+                self.update_w(total_dw/self.batch_size)
+                
+            numpy.random.shuffle(data)
 
-        
     def predict_proba(self, X):
         if X != numpy.ndarray:
             X = numpy.array(X)
@@ -106,4 +135,3 @@ class SparseLogisticRegression(object):
                 classes.append(0)
                 
         return classes
-    
